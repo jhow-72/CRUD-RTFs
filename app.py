45f817ae67a1adfc1404b968817efb23dd080a94
jhow-72 <jhow_72@usp.br>
2022-06-11 15:17:35 -0300

    criacao de novos HTML's

--- app.py
+++ app.py
@@ -15,5 +15,13 @@ def add():
 def edit():
     return render_template("editar.html")
 
+@app.route("/login", methods=['POST','GET'])
+def login():
+    return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
