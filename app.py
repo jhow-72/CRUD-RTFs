6da2f3e851f87fc940b428853d04471d16a22bca
Jhonatan Vinicius <jhow_72@usp.br>
2022-06-12 01:00:38 -0300

    pre CRUD state - finalizando estudos do flask basico

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 
 app = Flask(__name__)
-
+app.secret_key = "jhow"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def index():
@@ -17,11 +19,35 @@ def edit():
 
 @app.route("/login/", methods=['POST','GET'])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        session.permanent = True  # habilita o tempo setado no inicio do arquivo
+        user = request.form['nm']
+        session["user"] = user
+        flash(f"{user} logou com sucesso ")
+        return redirect(url_for('user'))
+    elif "user" in session:
+        user = session["user"]
+        flash(f"{user} já está logado ")
+        return redirect(url_for('user'))
+    else:
+        return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("Você não está logado!")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"{user} foi deslogado!", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     app.run(debug=True)
